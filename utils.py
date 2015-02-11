d06719882b35edb9e3dc8c7cae9a0aaa78bccadf
Matthias Kramm <kramm@google.com>
2015-02-11 09:01:18 -0800

    Towards exception support.

--- utils.py
+++ utils.py
@@ -13,3 +13,11 @@ from pytypedecl import pytd
 def GetDataFile(filename=""):
     return os.path.abspath(
         os.path.join(os.path.dirname(pytd.__file__), filename))
+
+
+def UnpackUnion(t):
+  """Return the type list for union type, or a list with the type itself."""
+  if isinstance(t, pytd.UnionType):
+    return t.type_list
+  else:
+    return [t]
